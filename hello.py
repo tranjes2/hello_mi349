6fb43ca7a891193876fa23722a35769f830ce657
Jessica Tran <tranjes2@msu.edu>
2021-09-13 13:05:50 -0400

    Small change to check Netlify deployment

---
+++ hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  5 20:50:57 2021
+
+@author: jessicatran
+"""
+print("Hello, World!")
---
+++ lab01.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# Laboratory Exercise #2 (Part D)
+#
+# Purpose:  compute the two roots of a quadratic equation.
+#
+# Import the math module to access function "math.sqrt()".
+
+import math
+
+A = float( input( "\nEnter the coefficient A: " ) )
+
+B = float( input( "\nEnter the coefficient B: " ) )
+
+C = float( input( "\nEnter the coefficient C: " ) )
+
+print( "\nThe coefficients of the equation:\n" )
+print( "  Coefficient A = ", A )
+print( "  Coefficient B = ", B )
+print( "  Coefficient C = ", C )
+
+
+# **** Replace the following with the calculations of the roots ****
+
+root1 = (-B + math.sqrt((B**2 )-4*A*C)) / (2*A)# replace 0.0 with the quadratic formula
+root2 = (-B - math.sqrt((B**2 )-4*A*C)) / (2*A)
+
+
+print( "\nThe roots of the equation:\n" )
+print( "  Root #1 = ", round(root1,3) )  # round the result to three decimal places before printing
+print( "  Root #2 = ", round(root2,3) )
